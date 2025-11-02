300c3da5ff820b16b00f77297e0970c9c13a0996
ViniciusVon <vinnyfut10@gmail.com>
2025-11-02 19:29:36 -0300

    Atualizando tema do frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PacientesComponent } from './components/paciente/paciente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'paciente', pathMatch: 'full' },
+    { path: 'paciente', component: PacientesComponent },
+];
