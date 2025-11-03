6d3c002d8cfb1ca3a53d39326b84b3be9e8de819
ViniciusVon <vinnyfut10@gmail.com>
2025-11-02 23:09:24 -0300

    Atualizando componente de Paciente para utilizar o Paciente tabs

--- frontend/src/app/components/paciente/paciente.component.ts
+++ frontend/src/app/components/paciente/paciente.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { PacienteListComponent } from '../paciente-list/paciente-list.component';
-import { PacienteDispositivosComponent } from '../paciente-dispositivos/paciente-dispositivos.component';
-import { PacienteHppComponent } from "../paciente-hpp/paciente-hpp.component";
-import { PacienteEvolucaoEnfermagemComponent } from "../paciente-evolucao-enfermagem/paciente-evolucao-enfermagem.component";
 import { CommonModule } from '@angular/common';
+import { PacienteTabsComponent } from '../paciente-tabs/paciente-tabs.component';
 
 @Component({
     selector: 'app-paciente',
     templateUrl: './paciente.component.html',
     styleUrls: ['./paciente.component.scss'],
     standalone: true,
-    imports: [PacienteListComponent, PacienteDispositivosComponent, CommonModule, PacienteHppComponent, PacienteEvolucaoEnfermagemComponent],
+    imports: [PacienteListComponent,
+        PacienteTabsComponent,
+        CommonModule,
+    ],
 })
 export class PacientesComponent {
     pacienteSelecionadoId: string | null = null;
